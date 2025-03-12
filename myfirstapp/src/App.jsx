025c5e6ff8c9af1d590eea50d4d98ff1233ac8d0
awaaidara <aidara.awa2020@gmail.com>
2025-03-12 11:49:18 +0000

    first commit

--- myfirstapp/src/App.jsx
+++ myfirstapp/src/App.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
 import Name from './components/name';
-// import Image from './components/image';
+ import Image from './components/image';
 import Description from './components/description';
+import Price from './components/price';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <card style={{ width: '18rem' }}>
-    <Card.Image variant="top"src="holder.js/100px180" />
+    <Card style={{ width: '18rem' }}>
+   <Image/>
     <Card.Body>
       <Card.Title><Name/></Card.Title>
-      <Card.TexT>
+      <Card.Text>
         <Description/>
-      </Card.TexT>
-     
+      </Card.Text>
+      <Price/>
       <Button variant="primary">Go somewhere</Button>
     </Card.Body>
-  </card>
+  </Card>
   );
 }
 
